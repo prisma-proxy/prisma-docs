cfacc821175c64685583d67b850760327cb36ca0
Yamimega <89793242+Yamimega@users.noreply.github.com>
2026-03-27 03:45:23 -0700

    chore: bump version to 2.27.0

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
           showLastUpdateTime: true,
           lastVersion: 'current',
           versions: {
-            current: {label: 'v2.26.0', path: ''},
+            current: {label: 'v2.27.0', path: ''},
           },
         },
         blog: false,
